a996a666c767ee2bad5fa5bf1b49aee6d837595c
abhimanyu <abhimanew2000@gmail.com>
2024-03-06 10:06:06 +0530

    modified project asgi

--- backend/project/notification/routing.py
+++ backend/project/notification/routing.py
@@ -3,7 +3,7 @@ from django.urls import re_path
 from . import consumers
 from .consumers import NotificationConsumer
 
-websocket_urlpatterns1 = [
+websocket_urlpatterns= [
     re_path(r"ws/notification/(?P<room_name>\w+)/$", consumers.NotificationConsumer.as_asgi()),
     re_path(r"ws/notification/", NotificationConsumer.as_asgi()),
 
