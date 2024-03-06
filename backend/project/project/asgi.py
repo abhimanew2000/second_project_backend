dc0ef34678ac5d024962d92a8e8f4504d6d39d4a
abhimanyu <abhimanew2000@gmail.com>
2024-03-06 14:52:30 +0530

    modified asgi

--- backend/project/project/asgi.py
+++ backend/project/project/asgi.py
@@ -1,14 +1,14 @@
 
 import os
 import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from notification.routing import websocket_urlpatterns as notification_websocket_urlpatterns
 from chat.routing import websocket_urlpatterns as chat_websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
